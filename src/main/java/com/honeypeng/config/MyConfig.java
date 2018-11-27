4a0ef37c33ebd809d9a050b75ec9f3d4bcd791de
pwx <1499129097@qq.com>
2018-11-27 18:34:28 +0800

    国际化

--- src/main/java/com/honeypeng/config/MyConfig.java
+++ src/main/java/com/honeypeng/config/MyConfig.java
@@ -1,13 +1,16 @@
 package com.honeypeng.config;
 
+import com.honeypeng.component.MyLocalResolver;
 import com.honeypeng.service.IHelloService;
 import com.honeypeng.service.impl.HelloserviceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
+import org.springframework.web.servlet.LocaleResolver;
 
 import javax.jms.ConnectionFactory;
+import java.util.Locale;
 
 /**
  * Created by 08754 on 2018/9/19.
@@ -28,5 +31,17 @@ public class MyConfig {
         factory.setConnectionFactory(connectionFactory);
         factory.setPubSubDomain(true);
         return factory;
+    }
+
+    /**
+     * 覆盖默认国际化设置
+     * @author pengWX
+     * @date 2018/11/27 17:25
+     * @param @param :
+     * @return org.springframework.web.servlet.LocaleResolver
+     */
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocalResolver();
     }
 }
